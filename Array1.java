d6b2b78c5d6a2231ba29c70a0193d6e4ff1ecade
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 08:18:48 +0530

    Array in java

---
+++ Array1.java
@@ -0,0 +1,8 @@
+package collection_framework_and_map;
+
+public class Array1 {
+    public static void main(String[] args) {
+        String[] car = { "BMW", "VOLVO", "VENAU", "THAR", "WAGOAR", "MG" };
+        System.out.println(car[1]);
+    }
+}
