32fb4fa25e16f84471ff272bbf2244ed4d3b684f
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 08:44:33 +0530

    iterator in arraylist

--- Iterator.java
+++ Iterator.java
@@ -1,5 +1,21 @@
 package collection_framework_and_map;
 
-public enum Iterator {
+import java.util.ArrayList;
 
+public class Iterator {
+     public static void main(String[] args) {
+
+    // Make a collection
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+
+    // Get the iterator
+    java.util.Iterator<String> it = cars.iterator();
+
+    // Print the first item
+    System.out.println(it.next());
+  }
 }
